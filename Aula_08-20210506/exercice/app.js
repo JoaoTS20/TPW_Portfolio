51d9d640bc78e4898b6c7ce0df352abca0236c28
JoaoTS20 <joaots@ua.pt>
2021-05-06 15:35:51 +0100

    Aula08

---
+++ Aula_08-20210506/exercice/app.js
@@ -0,0 +1,124 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Animal = /** @class */ (function () {
+    function Animal(n) {
+        this.habitat = n;
+        Animal.nAnimals++;
+    }
+    Animal.prototype.show = function () {
+        return "Habitat: " + this.habitat + " Number of Animals: " + Animal.nAnimals;
+    };
+    Animal.nAnimals = 0;
+    return Animal;
+}());
+var Reptile = /** @class */ (function (_super) {
+    __extends(Reptile, _super);
+    function Reptile(n) {
+        return _super.call(this, n) || this;
+    }
+    return Reptile;
+}(Animal));
+var Mammal = /** @class */ (function (_super) {
+    __extends(Mammal, _super);
+    function Mammal(n) {
+        var _this = _super.call(this, n) || this;
+        Mammal.nMammals++;
+        return _this;
+    }
+    Mammal.prototype.show = function () {
+        return _super.prototype.show.call(this) + " Number of Mammals: " + Mammal.nMammals;
+    };
+    Mammal.prototype.talk = function () {
+        return "Is talking: ";
+    };
+    Mammal.nMammals = 0;
+    return Mammal;
+}(Animal));
+var Canine = /** @class */ (function (_super) {
+    __extends(Canine, _super);
+    function Canine(n, race) {
+        var _this = _super.call(this, n) || this;
+        _this.race = race;
+        Canine.nCanines++;
+        return _this;
+    }
+    Canine.prototype.show = function () {
+        return _super.prototype.show.call(this) + " Race: " + this.race;
+    };
+    Canine.nCanines = 0;
+    return Canine;
+}(Mammal));
+var Dog = /** @class */ (function (_super) {
+    __extends(Dog, _super);
+    function Dog(n, race, bark) {
+        var _this = _super.call(this, n, race) || this;
+        _this.bark = bark;
+        Dog.nDogs++;
+        return _this;
+    }
+    Dog.prototype.talk = function () {
+        return _super.prototype.talk.call(this) + this.bark;
+    };
+    Dog.nDogs = 0;
+    return Dog;
+}(Canine));
+var Feline = /** @class */ (function (_super) {
+    __extends(Feline, _super);
+    function Feline(n, family) {
+        var _this = _super.call(this, n) || this;
+        _this.familiy = family;
+        Feline.nFelines++;
+        return _this;
+    }
+    Feline.prototype.show = function () {
+        return _super.prototype.show.call(this) + " Family: " + this.familiy;
+    };
+    Feline.nFelines = 0;
+    return Feline;
+}(Mammal));
+var Cat = /** @class */ (function (_super) {
+    __extends(Cat, _super);
+    function Cat(n, family, meow, name) {
+        var _this = _super.call(this, n, family) || this;
+        _this.meow = meow;
+        _this.name = name;
+        Cat.nCats++;
+        return _this;
+    }
+    Cat.prototype.talk = function () {
+        return _super.prototype.talk.call(this) + this.meow;
+    };
+    Cat.prototype.show = function () {
+        return _super.prototype.show.call(this) + " Name: " + this.name;
+    };
+    Cat.nCats = 0;
+    return Cat;
+}(Feline));
+console.log("Reptile:");
+var reptile = new Reptile("Lago");
+console.log(reptile.show());
+console.log("Dog:");
+var dog = new Dog('house', 'mix', 'woof woof!');
+var ss = dog.talk();
+console.log(ss);
+var sh = dog.show();
+console.log(sh);
+console.log("Cat:");
+var cat = new Cat('house', 'caty', 'meow meow!', 'Gatão');
+var css = cat.talk();
+console.log(css);
+var csh = cat.show();
+console.log(csh);
---
+++ Aula_08-20210506/exercice/app.ts
@@ -0,0 +1,122 @@
+class Animal {
+    static nAnimals: number=0;
+    habitat: string;
+
+    constructor(n: string){
+        this.habitat = n;
+        Animal.nAnimals++;
+    }
+
+    show() {
+        return "Habitat: "+ this.habitat + " Number of Animals: "+ Animal.nAnimals;
+    }
+}
+
+class Reptile extends Animal{
+
+    constructor(n: string){
+        super(n);
+    }
+
+}
+
+class Mammal extends Animal{
+    static nMammals: number=0;
+    constructor(n: string){
+        super(n);
+        Mammal.nMammals++;
+    }
+
+    show() {
+        return super.show() + " Number of Mammals: "+ Mammal.nMammals;
+    }
+
+    talk(){
+        return "Is talking: ";
+    }
+
+}
+
+class Canine extends Mammal{
+    static nCanines: number=0;
+    race: string;
+
+    constructor(n: string, race: string){
+        super(n);
+        this.race=race;
+        Canine.nCanines++;
+    }
+    show() {
+        return super.show() + " Race: " + this.race;
+    }
+
+}
+
+class Dog extends Canine{
+    static nDogs: number=0;
+    bark: string;
+
+    constructor(n: string, race: string, bark: string){
+        super(n,race);
+        this.bark=bark;
+        Dog.nDogs++;
+    }
+
+    talk() {
+        return super.talk() + this.bark;
+    }
+
+
+}
+
+
+class Feline extends Mammal{
+    static nFelines: number=0;
+    familiy: string;
+
+    constructor(n: string, family: string){
+        super(n);
+        this.familiy=family;
+        Feline.nFelines++;
+    }
+    show() {
+        return super.show() + " Family: " + this.familiy;
+    }
+}
+
+class Cat extends Feline{
+    static nCats: number=0;
+    meow: string;
+    name:string;
+    constructor(n: string, family: string, meow: string, name : string){
+        super(n,family);
+        this.meow=meow;
+        this.name=name;
+        Cat.nCats++;
+    }
+
+    talk() {
+        return super.talk() + this.meow;
+    }
+    show() {
+        return super.show()  + " Name: " + this.name;
+    }
+}
+
+console.log("Reptile:")
+let reptile = new Reptile("Lago")
+console.log(reptile.show());
+console.log("Dog:")
+let dog = new Dog('house', 'mix', 'woof woof!')
+let ss = dog.talk()
+console.log(ss)
+let sh = dog.show()
+console.log(sh)
+console.log("Cat:")
+let cat = new Cat('house', 'caty', 'meow meow!' , 'Gatão')
+let css = cat.talk()
+console.log(css)
+let csh = cat.show()
+console.log(csh)
+
+
